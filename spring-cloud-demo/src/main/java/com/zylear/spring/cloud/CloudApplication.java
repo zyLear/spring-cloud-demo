72b6b359003b2cdaed037d3205c4d4082ab4403f
xiezongyu <zongyu.xie@magnumwm.com>
2020-07-31 17:08:01 +0800

    run

--- spring-cloud-demo/src/main/java/com/zylear/spring/cloud/CloudApplication.java
+++ spring-cloud-demo/src/main/java/com/zylear/spring/cloud/CloudApplication.java
@@ -2,12 +2,14 @@ package com.zylear.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CloudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(CloudApplication.class, args);
+
     }
 
 }
