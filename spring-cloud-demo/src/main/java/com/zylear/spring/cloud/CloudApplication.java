8a83a4fb2a619a6e58758d4fb20995bb6b0df678
xiezongyu <zongyu.xie@magnumwm.com>
2020-08-12 18:40:03 +0800

    tst

--- spring-cloud-demo/src/main/java/com/zylear/spring/cloud/CloudApplication.java
+++ spring-cloud-demo/src/main/java/com/zylear/spring/cloud/CloudApplication.java
@@ -4,15 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class CloudApplication extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(CloudApplication.class);
+
+
+//    @Bean
+//    public String redisTemplate(Integer oneo){
+//        return "x";
+//    }
 
     static {
         logger.info("CloudApplication static test");
