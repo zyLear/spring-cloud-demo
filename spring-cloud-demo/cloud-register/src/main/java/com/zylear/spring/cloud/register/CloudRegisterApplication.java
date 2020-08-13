15f5f4c2458bbaaa7bcd0b48678fd6e5075bff8e
zylear <284449710@qq.com>
2020-08-13 17:52:05 +0800

    startupcloud

--- spring-cloud-demo/cloud-register/src/main/java/com/zylear/spring/cloud/register/CloudRegisterApplication.java
+++ spring-cloud-demo/cloud-register/src/main/java/com/zylear/spring/cloud/register/CloudRegisterApplication.java
@@ -1,5 +1,7 @@
 package com.zylear.spring.cloud.register;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,8 +10,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class CloudRegisterApplication extends SpringBootServletInitializer {
 
+    public static final Logger logger = LoggerFactory.getLogger(CloudRegisterApplication.class);
+
+    static {
+        logger.info("start.");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CloudRegisterApplication.class, args);
+        logger.info("startup success.");
     }
 
     @Override
