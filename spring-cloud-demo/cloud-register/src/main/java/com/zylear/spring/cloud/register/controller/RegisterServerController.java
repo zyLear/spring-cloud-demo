9abd6829b69c25763cf37607a9e093119283c22e
zylear <284449710@qq.com>
2023-06-01 23:09:45 +0800

    visualvm heap dume test, find big objects GC root

--- spring-cloud-demo/cloud-register/src/main/java/com/zylear/spring/cloud/register/controller/RegisterServerController.java
+++ spring-cloud-demo/cloud-register/src/main/java/com/zylear/spring/cloud/register/controller/RegisterServerController.java
@@ -6,13 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/server")
 public class RegisterServerController {
+
+    List list = new ArrayList();
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("hello", HttpStatus.OK);
     }
+
+//    @PostConstruct
+//    private void init() throws InterruptedException {
+//        for (int i = 0; i < 50; i++) {
+//            list.add(new byte[1024*1024]);
+//            Thread.sleep(500);
+//            System.out.println("add");
+//        }
+//    }
 
 }
